5c4f23fd73fd2e7d828417fbb948961147ac4b34
Adhytia Tungga <adhytia.tungga@it.sharingvision.com>
2024-09-20 08:44:49 +0700

    feat: modified server

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,21 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
