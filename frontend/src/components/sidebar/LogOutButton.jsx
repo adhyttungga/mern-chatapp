75fe718af08e5d6c4f57916b068488dcb9088425
Adhytia Tungga <adhytia.tungga@it.sharingvision.com>
2024-09-27 00:56:26 +0700

    feat: add home page

---
+++ frontend/src/components/sidebar/LogOutButton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BiLogOut } from "react-icons/bi";
+
+const LogOutButton = () => {
+  return (
+    <div className="mt-auto">
+      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+    </div>
+  );
+};
+
+export default LogOutButton;
+
+// STARTER CODE FOR THIS FILE
+// import React from "react";
+// import { BiLogOut } from "react-icons/bi";
+
+// const LogOutButton = () => {
+//   return (
+//     <div className="mt-auto">
+//       <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+//     </div>
+//   );
+// };
+
+// export default LogOutButton;
---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import SearchInput from "./SearchInput";
+import Conversations from "./Conversations";
+import LogOutButton from "./LogOutButton";
+
+const Sidebar = () => {
+  return (
+    <div className="border-r border-slate-500 p-4flex flex-col">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      <LogOutButton />
+    </div>
+  );
+};
+
+export default Sidebar;
+
+// STARTER CODE FOR THIS FILE
+// import React from "react";
+// import SearchInput from "./SearchInput";
+// import Conversations from "./Conversations";
+// import LogOutButton from "./LogOutButton";
+
+// const Sidebar = () => {
+//   return (
+//     <div className="border-r border-slate-500 p-4flex flex-col">
+//       <SearchInput />
+//       <div className="divider px-3"></div>
+//       <Conversations />
+//       <LogOutButton />
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
