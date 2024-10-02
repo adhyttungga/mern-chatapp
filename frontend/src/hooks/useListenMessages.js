806e95569f50105427642ab1cb3a93c35d18a30a
cap7nemo <adhytiatungga09031996@gmail.com>
2024-10-02 15:35:51 +0700

    feat: add socket client

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useSocketContext } from "../contexts/SocketContext";
+import useConversation from "../zustand/useConversation";
+import notificationSound from "../assets/sounds/notification.wav";
+
+const useListenMessages = () => {
+  const { socket } = useSocketContext();
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      newMessage.shouldShake = true;
+
+      if (selectedConversation._id === newMessage.senderId) {
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      }
+    });
+
+    return () => {
+      socket?.off("newMessage");
+    };
+  }, [socket, setMessages, messages]);
+};
+
+export default useListenMessages;
